0be6bcdc349cb85ebc6a68b94f3bd83bf4201e37
MikkeGF <miikkael.peltokorpi@icloud.com>
2020-03-31 13:18:28 +0300

    headerin tekoa

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -12,6 +12,26 @@
   </head>
   <body>
   <div class="container">
+  <div class="row">
+      <div class="col-md-4 text-center">
+        <img src="/../images/logo.png" alt="logo">
+      </div>
+      <div class="col-md-4 d-flex justify-content-center">
+        <form action="">
+        <div class="input-group md-form form-sm form-2 pl-0">
+  <input class="form-control my-0 py-1 red-border" type="text" placeholder="Search" aria-label="Search">
+  <div class="input-group-append">
+    <span class="input-group-text red lighten-3" id="basic-text1"><i class="fas fa-search text-grey"
+        aria-hidden="true"></i></span>
+  </div>
+</div>
+        </form>
+      </div>
+      <div class="col-md-4 text-center">
+      <p href="">jotain</p>
+      <p href="">jotain</p>
+      </div>
+  </div>
         <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
         
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -70,10 +90,7 @@
               </li>
             </ul>
             <?php echo '<div><a href="' . site_url('cart/index') . '">Ostoskori<a/></div>' ?>
-             <form class="form-inline my-2 my-lg-0">
-              <input class="form-control mr-sm-2" type="" placeholder="Etsi tuotteita" aria-label="Etsi">
-              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Etsi Tuotteita</button>
-            </form>
+             
           </div>
       </nav>
 
