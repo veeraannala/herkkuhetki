2cccc18e38e98cec7c7934ba91e6d2fcefaf6de4
Jonne Tuohimaa <n9tujo00@students.oamk.fi>
2020-03-31 13:31:33 +0300

    etusivun tekeminen

--- app/Views/front_page.php
+++ app/Views/front_page.php
@@ -1,192 +1,62 @@
-<div class="card mt-3 mb-1" style="width: 15rem;">
+<div class="row mb-5 pt-5">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="images/karkki.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/karkki1.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/karkki2.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+<div class="row">
+<div class="col-lg-3 col-md-4 card mt-3 mb-1">
   <a href="/show_product" ><img class="card-img-top" src="images/lolly.jpg" alt="Card image cap"></a>
   <div class="card-body text-center">
     <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
     <p class="card-text">Ostappa halpoja tikkareita!</p>
-    <a href="cart/insert" class="btn btn-primary">Lisää ostoskoriin</a>
+    <a href="cart/insert" class="btn btn-danger">Lisää ostoskoriin</a>
   </div>
 </div>
-<div class="card mt-3 mb-1" style="width: 15rem;">
-  <img class="card-img-top" src="images/lolly.jpg" alt="Card image cap">
+<div class="col-lg-3 col-md-4 card mt-3 mb-1">
+  <a href="/show_product" ><img class="card-img-top" src="images/lolly.jpg" alt="Card image cap"></a>
   <div class="card-body text-center">
     <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
     <p class="card-text">Ostappa halpoja tikkareita!</p>
-    <a href="#" class="btn btn-primary">Lisää ostoskoriin</a>
+    <a href="cart/insert" class="btn btn-danger">Lisää ostoskoriin</a>
   </div>
 </div>
-<div class="card mt-3 mb-1" style="width: 15rem;">
-  <img class="card-img-top" src="images/lolly.jpg" alt="Card image cap">
+<div class="col-lg-3 col-md-4 card mt-3 mb-1">
+  <a href="/show_product" ><img class="card-img-top" src="images/lolly.jpg" alt="Card image cap"></a>
   <div class="card-body text-center">
     <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
     <p class="card-text">Ostappa halpoja tikkareita!</p>
-    <a href="#" class="btn btn-primary">Lisää ostoskoriin</a>
+    <a href="cart/insert" class="btn btn-danger">Lisää ostoskoriin</a>
   </div>
 </div>
-<div class="card mt-3 mb-1" style="width: 15rem;">
-  <img class="card-img-top" src="images/lolly.jpg" alt="Card image cap">
+<div class="col-lg-3 col-md-4 card mt-3 mb-1">
+  <a href="/show_product" ><img class="card-img-top" src="images/lolly.jpg" alt="Card image cap"></a>
   <div class="card-body text-center">
     <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
     <p class="card-text">Ostappa halpoja tikkareita!</p>
-    <a href="#" class="btn btn-primary">Lisää ostoskoriin</a>
+    <a href="cart/insert" class="btn btn-danger">Lisää ostoskoriin</a>
   </div>
 </div>
-<div class="card mt-3 mb-1" style="width: 15rem;">
-  <img class="card-img-top" src="images/lolly.jpg" alt="Card image cap">
-  <div class="card-body text-center">
-    <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
-    <p class="card-text">Ostappa halpoja tikkareita!</p>
-    <a href="#" class="btn btn-primary">Lisää ostoskoriin</a>
-  </div>
-</div>
-<div class="card mt-3 mb-1" style="width: 15rem;">
-  <img class="card-img-top" src="images/lolly.jpg" alt="Card image cap">
-  <div class="card-body text-center">
-    <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
-    <p class="card-text">Ostappa halpoja tikkareita!</p>
-    <a href="#" class="btn btn-primary">Lisää ostoskoriin</a>
-  </div>
-</div>
-<div class="card mt-3 mb-1" style="width: 15rem;">
-  <img class="card-img-top" src="images/lolly.jpg" alt="Card image cap">
-  <div class="card-body text-center">
-    <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
-    <p class="card-text">Ostappa halpoja tikkareita!</p>
-    <a href="#" class="btn btn-primary">Lisää ostoskoriin</a>
-  </div>
-</div>
-<div class="card mt-3 mb-1" style="width: 15rem;">
-  <img class="card-img-top" src="images/lolly.jpg" alt="Card image cap">
-  <div class="card-body text-center">
-    <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
-    <p class="card-text">Ostappa halpoja tikkareita!</p>
-    <a href="#" class="btn btn-primary">Lisää ostoskoriin</a>
-  </div>
-</div>
-<div class="card mt-3 mb-1" style="width: 15rem;">
-  <img class="card-img-top" src="images/lolly.jpg" alt="Card image cap">
-  <div class="card-body text-center">
-    <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
-    <p class="card-text">Ostappa halpoja tikkareita!</p>
-    <a href="#" class="btn btn-primary">Lisää ostoskoriin</a>
-  </div>
-</div>
-<div class="card mt-3 mb-1" style="width: 15rem;">
-  <img class="card-img-top" src="images/lolly.jpg" alt="Card image cap">
-  <div class="card-body text-center">
-    <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
-    <p class="card-text">Ostappa halpoja tikkareita!</p>
-    <a href="#" class="btn btn-primary">Lisää ostoskoriin</a>
-  </div>
-</div>
-<div class="card mt-3 mb-1" style="width: 15rem;">
-  <img class="card-img-top" src="images/lolly.jpg" alt="Card image cap">
-  <div class="card-body text-center">
-    <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
-    <p class="card-text">Ostappa halpoja tikkareita!</p>
-    <a href="#" class="btn btn-primary">Lisää ostoskoriin</a>
-  </div>
-</div>
-<div class="card mt-3 mb-1" style="width: 15rem;">
-  <img class="card-img-top" src="images/lolly.jpg" alt="Card image cap">
-  <div class="card-body text-center">
-    <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
-    <p class="card-text">Ostappa halpoja tikkareita!</p>
-    <a href="#" class="btn btn-primary">Lisää ostoskoriin</a>
-  </div>
-</div>
-<div class="card mt-3 mb-1" style="width: 15rem;">
-  <img class="card-img-top" src="images/lolly.jpg" alt="Card image cap">
-  <div class="card-body text-center">
-    <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
-    <p class="card-text">Ostappa halpoja tikkareita!</p>
-    <a href="#" class="btn btn-primary">Lisää ostoskoriin</a>
-  </div>
-</div>
-<div class="card mt-3 mb-1" style="width: 15rem;">
-  <img class="card-img-top" src="images/lolly.jpg" alt="Card image cap">
-  <div class="card-body text-center">
-    <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
-    <p class="card-text">Ostappa halpoja tikkareita!</p>
-    <a href="#" class="btn btn-primary">Lisää ostoskoriin</a>
-  </div>
-</div>
-<div class="card mt-3 mb-1" style="width: 15rem;">
-  <img class="card-img-top" src="images/lolly.jpg" alt="Card image cap">
-  <div class="card-body text-center">
-    <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
-    <p class="card-text">Ostappa halpoja tikkareita!</p>
-    <a href="#" class="btn btn-primary">Lisää ostoskoriin</a>
-  </div>
-</div>
-<div class="card mt-3 mb-1" style="width: 15rem;">
-  <img class="card-img-top" src="images/lolly.jpg" alt="Card image cap">
-  <div class="card-body text-center">
-    <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
-    <p class="card-text">Ostappa halpoja tikkareita!</p>
-    <a href="#" class="btn btn-primary">Lisää ostoskoriin</a>
-  </div>
-</div>
-<div class="card mt-3 mb-1" style="width: 15rem;">
-  <img class="card-img-top" src="images/lolly.jpg" alt="Card image cap">
-  <div class="card-body text-center">
-    <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
-    <p class="card-text">Ostappa halpoja tikkareita!</p>
-    <a href="#" class="btn btn-primary">Lisää ostoskoriin</a>
-  </div>
-</div>
-<div class="card mt-3 mb-1" style="width: 15rem;">
-  <img class="card-img-top" src="images/lolly.jpg" alt="Card image cap">
-  <div class="card-body text-center">
-    <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
-    <p class="card-text">Ostappa halpoja tikkareita!</p>
-    <a href="#" class="btn btn-primary">Lisää ostoskoriin</a>
-  </div>
-</div>
-<div class="card mt-3 mb-1" style="width: 15rem;">
-  <img class="card-img-top" src="images/lolly.jpg" alt="Card image cap">
-  <div class="card-body text-center">
-    <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
-    <p class="card-text">Ostappa halpoja tikkareita!</p>
-    <a href="#" class="btn btn-primary">Lisää ostoskoriin</a>
-  </div>
-</div>
-<div class="card mt-3 mb-1" style="width: 15rem;">
-  <img class="card-img-top" src="images/lolly.jpg" alt="Card image cap">
-  <div class="card-body text-center">
-    <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
-    <p class="card-text">Ostappa halpoja tikkareita!</p>
-    <a href="#" class="btn btn-primary">Lisää ostoskoriin</a>
-  </div>
-</div>
-<div class="card mt-3 mb-1" style="width: 15rem;">
-  <img class="card-img-top" src="images/lolly.jpg" alt="Card image cap">
-  <div class="card-body text-center">
-    <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
-    <p class="card-text">Ostappa halpoja tikkareita!</p>
-    <a href="#" class="btn btn-primary">Lisää ostoskoriin</a>
-  </div>
-</div>
-<div class="card mt-3 mb-1" style="width: 15rem;">
-  <img class="card-img-top" src="images/lolly.jpg" alt="Card image cap">
-  <div class="card-body text-center">
-    <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
-    <p class="card-text">Ostappa halpoja tikkareita!</p>
-    <a href="#" class="btn btn-primary">Lisää ostoskoriin</a>
-  </div>
-</div>
-<div class="card mt-3 mb-1" style="width: 15rem;">
-  <img class="card-img-top" src="images/lolly.jpg" alt="Card image cap">
-  <div class="card-body text-center">
-    <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
-    <p class="card-text">Ostappa halpoja tikkareita!</p>
-    <a href="#" class="btn btn-primary">Lisää ostoskoriin</a>
-  </div>
-</div>
-<div class="card mt-3 mb-1" style="width: 15rem;">
-  <img class="card-img-top" src="images/lolly.jpg" alt="Card image cap">
-  <div class="card-body text-center">
-    <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
-    <p class="card-text">Ostappa halpoja tikkareita!</p>
-    <a href="#" class="btn btn-primary">Lisää ostoskoriin</a>
-  </div>
 </div>
