f82255e388dd0c2db5dacaff5ffb9170294002d8
MikkeGF <miikkael.peltokorpi@icloud.com>
2020-03-31 11:56:31 +0300

    perustason navbar

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -11,44 +11,70 @@
     <title></title>
   </head>
   <body>
-    <div class="container">
-    <div>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+  <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+        <a class="navbar-brand" href="#">Herkkuhetki</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
-    <?php echo "<div>0</div>"; ?>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-  </div>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Irtokarkit
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Salmiakit</a>
+                  <a class="dropdown-item" href="#">Kirpeät karkit</a>
+                  <a class="dropdown-item" href="#">Kovat karkit</a>
+                  <a class="dropdown-item" href="#">Lakritsit</a>
+                  <a class="dropdown-item" href="#">Vaahtokarkit</a>
+                  <a class="dropdown-item" href="#">Toffeet</a>
+                  <a class="dropdown-item" href="#">Viinikumit</a>
+                  
+                </div>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Pakatut makeiset
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Karkkipussit</a>
+                  <a class="dropdown-item" href="#">Karkkilaatikot</a>
+                  <a class="dropdown-item" href="#">Lakupatukat</a>
+                </div>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Suklaat
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Suklaalevyt</a>
+                  <a class="dropdown-item" href="#">Suklaapatukat</a> 
+                  <a class="dropdown-item" href="#">Suklaamunat</a>
+                </div>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Teemakarkit
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Joulu</a>
+                  <a class="dropdown-item" href="#">Pääsiäinen</a>
+                  <a class="dropdown-item" href="#">Halloweenkarkit</a>
+                  <a class="dropdown-item" href="#">Ystävänpäivä</a>
+                </div>
+              </li>
+            </ul>
+             <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="" placeholder="Etsi tuotteita" aria-label="Etsi">
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Etsi Tuotteita</button>
+            </form>
+          </div>
+      </nav>
+    </div>
+      <div class="container">
 
    
