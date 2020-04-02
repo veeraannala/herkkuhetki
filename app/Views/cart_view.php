db68fc23800381f431d170b85ddbda0ef41d83db
EmmiL <c9laem00@students.oamk.fi>
2020-04-02 15:02:04 +0300

    otsikko ostoskoriin

--- app/Views/cart_view.php
+++ app/Views/cart_view.php
@@ -3,13 +3,15 @@
 if(is_array($purchases) && count($purchases)>0)
 {
 ?>
+    <h3>Ostoskori</h3>
     <?php echo '<a href="' . site_url('cart/clear') . '"> Tyhjennä</a>'; ?>
     <?php foreach ($purchases as $purchase): ?>
         <p style="margin-bottom: 0 !important"><?= $purchase?></p>
        <?php endforeach;?>
 <?php
-}else{
-   echo '<div><p style="margin-bottom: 0 !important">Ostoskorisi on tyhjä jatka ostoksille <a href="' . site_url('') . '">tästä<a/></p></div>';
+}else{ ?>
+    <h3>Ostoskori</h3>
+    <?php echo '<div><p style="margin-bottom: 0 !important">Ostoskorisi on tyhjä jatka ostoksille <a href="' . site_url('') . '">tästä<a/></p></div>';
 }
 ?>
 </div>
