bdfab10cd1796b6da2be64e305d405ad6f95782f
EmmiL <c9laem00@students.oamk.fi>
2020-04-03 11:59:08 +0300

    ostoskorin näkymän muokkausta

--- app/Views/cart_view.php
+++ app/Views/cart_view.php
@@ -3,15 +3,15 @@
 if(is_array($purchases) && count($purchases)>0)
 {
 ?>
-    <h3>Ostoskori</h3>
+    <h3 class="pt-4">Ostoskori</h3>
     <?php echo '<a href="' . site_url('cart/clear') . '"> Tyhjennä</a>'; ?>
     <?php foreach ($purchases as $purchase): ?>
-        <p style="margin-bottom: 0 !important"><?= $purchase?></p>
+        <p class="mt-4" style="margin-bottom: 0 !important"><?= $purchase?></p>
        <?php endforeach;?>
 <?php
 }else{ ?>
-    <h3>Ostoskori</h3>
-    <?php echo '<div><p style="margin-bottom: 0 !important">Ostoskorisi on tyhjä jatka ostoksille <a href="' . site_url('') . '">tästä<a/></p></div>';
+    <h3 class="pt-4">Ostoskori</h3>
+    <?php echo '<div class="mt-3"><p style="margin-bottom: 0 !important">Ostoskorisi on tyhjä jatka ostoksille <a href="' . site_url('') . '">tästä<a/></p></div>';
 }
 ?>
 </div>
