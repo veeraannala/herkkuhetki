ddde2ef9c339561e8ccc144411ba53d899f546fe
Jonne Tuohimaa <n9tujo00@students.oamk.fi>
2020-04-05 19:52:19 +0300

    etusivulle arvotaan 8. tuotetta esille

--- app/Views/product.php
+++ app/Views/product.php
@@ -1,8 +1,16 @@
 
 <div class="row">
-<?php foreach ($product as $prod): ?>
+<?php
+  $i = 0;
+  shuffle($product);
+  foreach ($product as $prod):{
+  $i++;
+  if ($i > 8) {
+  break;       
+}
+} ?>
     <div class="col-lg-3 col-md-4 card mt-3 mb-1">
-      <a href="<?=site_url('shop/show_product/' . $prod['id'])?>" ><img class="img-fluid" src="<?=base_url($prod['image'] . '.png')?>">
+      <a href="/show_product" ><img class="img-fluid" src="<?=base_url($prod['image'] . '.png')?>">
     <div class="card-body text-center">
       <h5 class="card-title"><?= $prod['name'] ?></h5>
       <p class="card-text"><?= $prod['price'] . '€/100G' ?></p></a>
