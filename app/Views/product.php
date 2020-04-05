665d7058a57dc94be3493114b2ca6f46d606f3e9
Jonne Tuohimaa <n9tujo00@students.oamk.fi>
2020-04-05 11:24:57 +0300

    kortin tulostuksen muokkaus

--- app/Views/product.php
+++ app/Views/product.php
@@ -2,13 +2,13 @@
 <div class="row">
 <?php foreach ($product as $prod): ?>
     <div class="col-lg-3 col-md-4 card mt-3 mb-1">
-      <a href="/show_product" ><img class="img-fluid" src="<?=base_url($prod['image'] . '.png')?>"></a>
+      <a href="/show_product" ><img class="img-fluid" src="<?=base_url($prod['image'] . '.png')?>">
     <div class="card-body text-center">
-      <h5 class="card-title">Ostappa halpoja tikkareita!</h5>
-      <p class="card-text">Ostappa halpoja tikkareita!</p>
+      <h5 class="card-title"><?= $prod['name'] ?></h5>
+      <p class="card-text"><?= $prod['price'] . '€/100G' ?></p></a>
       <form method="post" action="<?= site_url('cart/insert')?>">
       <input type="hidden" name="product" value="200">
-      <button class="btn">Lisää ostoskoriin</button>
+      <button class="btn mt-2">Lisää ostoskoriin</button>
       </form>
     </div>
   </div>
