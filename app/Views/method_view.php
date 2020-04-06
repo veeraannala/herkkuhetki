1e166eb901100abcbf3c40bf48ac88b86aed5743
EmmiL <c9laem00@students.oamk.fi>
2020-04-06 11:35:54 +0300

    toimitustavat ja maksutavat sivu tehty

--- app/Views/method_view.php
+++ app/Views/method_view.php
@@ -1,4 +1,86 @@
 <div class="methodminheight">
-<h3>Toimii</h3>
-
+    <h1>Maksu- ja toimitusehdot</h1>
+        
+    <h2>Toimitus</h2>
+        <p>Hyvä asiakas, saat vahvistuksen tilauksestasi sähköpostiin(ja muilla haluamillasi tavoilla) heti tilauksen jälkeen.
+            Lähetämme tilauksesi 1-4 arkipäivän kuluessa!
+        </p>
+        <p>Toimitustavat ja toimituskulut (€):</p>
+        <p>Nouto (sovitulta paikalta)</p>
+        <p>Toimitus lähimpään postin toimituspaikkaan 5€ per lähetys</p>
+        <p>Postin kotiinkuljetus  100,00 € per lähetys.</p>
+        <p>Muun kuljetuspalvelun kotiinkuljetus  10,00 € per lähetys.</p>
+        <p>Yli 80 € ostokset veloituksetta lähipostiisi.</p>
+        <p>Yli 200 € ostokset veloituksetta kotiovellesi.</p>
+    <h3>Nouto postista</h3>
+        <p>Toimitetaan lähimpään postiin, 
+            josta voit paketin noutaa saatuasi saapumisilmoituksen. 
+            Saat ohjeet noutamiseen tekstiviestillä paketin saapuessa. 
+            Henkilöllisyys pitää todistaa noudon yhteydessä!</p>
+    <h3>pakettiautomaatista noutaminen</h3>
+        <p>Nouda tilauksesi valitsemastasi pakettiautomaatista.  
+            Saat ohjeet noutamiseen (tekstiviestillä tai sähköpostilla) paketin saapuessa.
+             Muista tarkistaa pakettiautomaatin sijainti ennen kuin menet sinne!</p>
+    <h3>Kotiinkuljetus</h3>
+        <p>
+        Paketin kotiinkuljetuksen takia kuljettaja ottaa sinuun yhteyttä, jotta kuljetuksen ajankohta voidaan sopia.
+        Kotiinkuljetus ajankohta on arkipäivisin klo 9-16
+        </p>
+        <p>
+        Valitettavasti emme voi välttämättä kuljettaa kotiovellesi jos asut keskellä ei mitään.
+        </p>
+        <p>
+        HUOM! Kun valitset tuotteelle kotiinkuljetuksen niin palvelu ei koske palautuksia.
+        Mahdollisessa palautus tilanteessa asiakkaan tulee itse toimittaa postin kautta palautettava paketti. 
+        </p>
+    <h3>Nouto</h3>
+        <p>Voit halutessasi noutaa tilauksesi varastoltamme tai sovitusta nouto paikasta. 
+            Saat ilmoituksen kun voit noutaa tilauksesi. Nouto on maksuton.</p>
+        <p>Henkilöllisyys pitää todistaa noudon yhteydessä!</p>
+    <h3>Toimutusaika</h3>
+        <p>tilauksesi lähetetään 1-4 arkipäivän kuluessa. 
+            Jos tuote on loppunut varastosta otamme sinuun yhteyttä mahdollisimman nopeasti.
+        Emme ole vastuussa Postin tai muiden kuljetus firmojen viiveistä tai mahdollisista muutoksista ja niiden seurauksista.</p>
+    <h3>Ostoskori</h3>
+        <p>Verkkokauppamme toimii ostoskori-periaatteella. Ostoskori näkyy aina selaimessa oikeassa ylälaidassa.</p>
+        <p>Ostoskorissa voit:<p>
+        <p>Poistaa tuotteita<p>
+        <p>Vaihtaa ostoskorissa olevan tuotteen määrää<p>
+        <p>Tyhjentää ostoskorin vain yhtä nappia painamalla!<p>
+        <p>Klikkaamalla ostoskorissa olevaa tuotetta pääset selaamaan tuotteen tietoja</p>
+        <p>Muista valita haluamasi maksu- ja toimitustapa tilauksen yhteydessä</p>
+    <h3>Vaihto-,palautus- ja peruutusoikeus</h3>
+        <p>Tästä verkkokaupasta ostamasi tuotteet ovat elintarvike-tuotteita, jolloin tuotteilla ei ole palautusoikeutta.
+            Jos tuotetta ei ole noudettu, niin tilaus on silti voimassa ja veloitamme uudelleen lähetyksestä 20€.
+        </p>
+        <p>Tietyissä tilanteissa kun tuote tai tilaus on viallinen, niin palautus tai vaihtaminen on maksutonta.</p>
+        <p>Mikäli paketti tai sen sisältö on kuljetuksessa vioittunut, tee siitä ilmoitus postille vastaanottaessasi pakettia, jonka jälkeen ota yhteys asiakaspalveluumme!</p>
+    <h3>Tilaaminen</h3>
+        <p>Verkkokauppaamme voit halutessasi rekisteröityä/kirjautua, mutta tilaaminen onnistuu myös ilman kirjautumista.
+            Kun olet lisännyt ostoskoriin kaiken haluamasi paina ostoskori painiketta selaimessa oikealla ylhäällä.
+            Varmista, että haluamasi tuotteet ovat ostoskorissa. Paina sitten tilaa painiketta ja täytä tilauslomakeeseen tarvittavat tiedot (toimitustapa, maksutapa,omat tiedot), 
+            jonka jälkeen voit maksaa ostoksesi. HUOM! hinnat ovat euroissa!        
+        </p>
+        <p>Tuotteiden saatavuustiedot löydät tuotteiden omilta sivuilta. 
+            Mikäli tilattua tuotetta ei ole varastossa otamme asiakkaaseen yhteyttä mahdollisimman pian ja korjaamme tilauksen asiakkaan haluamalla tavalla.
+            Kaikki tuotteitamme on saatavilla vain rajattu määrä.
+        </p>
+    <h3>Maksutavat</h3>
+        <p>Meillä voit maksaa:</p>
+        <p>Klarna</p>
+        <p>Verkkopankki</p>
+        <p>Lasku</p>
+        <p>Erämaksu</p>
+        <p>Luottokortti</p>
+        <p>Mobilepay</p>
+        <p>Kaikki tavat ovat yhtä turvallisia!</p>
+    <h3>Asiakastietojen säilyttäminen ja luovutus</h3>
+        <p>Käsittelemme kaikki asiakastiedot täysin luottamuksellisesti ja sitoudumme olemaan luovuttamatta asiakastietoja kolmannelle osapuolelle</p>
+    <h3>Kysyttävää?</h3>
+        <p>Ota yhteyttä:</p>
+        <p>Herkkuhetki OY</p>
+        <p>Jokutie 12</p>
+        <p>34234 Jossakin</p>
+        <p>Puh. 03012345678</p>
+        <p>Email: herkkuhetki@verkkokauppa.fi</p>
 </div>
