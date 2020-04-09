49e19dae72afa6d4307daf875a0fa01c573827f2
EmmiL <c9laem00@students.oamk.fi>
2020-04-09 15:59:23 +0300

    tuotteiden määrän lisääminen

---
+++ app/Views/admin/updateAmount_view.php
@@ -0,0 +1,27 @@
+<div class="row">
+    <div class="col">
+    <?php foreach ($products as $product): ?>
+        
+                <?php
+                if ($product['id'] === $id) {
+            ?>
+            <h3 class="mt-3 mb-3">Muokkaa tuotteen "<?=$product['name']?>" määrää varastossa</h3>
+        <div class="col-5">
+            <form method="post" action="<?= site_url('admin/update2')?>" class="mb-5">
+
+                <div class="form-row">
+                    <div class="col">
+                        <label class="m-3" for="newAmount">Uusi määrä: </label>
+                    </div>
+                    <div class="col">
+                        <input type="number" class="form-control" id="newAmount" name="newAmount"></input>
+                    </div>
+                </div>
+                <button>Muokkaa</button>
+                <?php } 
+    endforeach; ?>
+
+            </form>
+        </div>
+    </div>
+</div>
