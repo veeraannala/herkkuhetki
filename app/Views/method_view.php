41c2b0e3b183f5a0f42c8acb071759689881e053
Veera <n9anve00@students.oamk.fi>
2020-04-15 11:31:57 +0300

    toimitustapasivun viilaus

--- app/Views/method_view.php
+++ app/Views/method_view.php
@@ -1,46 +1,55 @@
 <div class="row">
-    <div class="col-12">
+    <div class="col-12 mt-3 mr-2">
 
         <div>
             <h2>Maksu- ja toimitusehdot</h2>
         </div>
         <div>
             <h3>Toimitus ja tuotteiden saatavuus</h3>
-            <p>Hyvä asiakkaamme,</p>
-            <p> saat vahvistuksen tilauksestasi sähköpostiisi heti tilauksen jälkeen.
-                Jos tuotetta on varastossa, lähetämme tilauksesi seuraavana arkipäivänä. Kun tilaamme tuotetta lisää
-                varastoomme lähetysaika on 5-8 arkipäivää. Tuotteiden saatavuustiedot löydät tuotteiden omilta sivuilta.
-                Mikäli tilattua tuotetta ei ole varastossa otamme asiakkaaseen yhteyttä mahdollisimman pian ja korjaamme
-                tilauksen asiakkaan haluamalla tavalla. Kaikki tuotteitamme on saatavilla vain rajattu määrä.</p>
-
+            <div class="ml-3">
+                <p>Hyvä asiakkaamme,</p>
+                <p> saat vahvistuksen tilauksestasi sähköpostiisi heti tilauksen jälkeen.
+                    Jos tuotetta on varastossa, lähetämme tilauksesi seuraavana arkipäivänä. Kun tilaamme tuotetta lisää
+                    varastoomme lähetysaika on 5-8 arkipäivää. Tuotteiden saatavuustiedot löydät tuotteiden omilta
+                    sivuilta.
+                    Mikäli tilattua tuotetta ei ole varastossa otamme asiakkaaseen yhteyttä mahdollisimman pian ja
+                    korjaamme
+                    tilauksen asiakkaan haluamalla tavalla. Kaikki tuotteitamme on saatavilla vain rajattu määrä.</p>
+            </div>
         </div>
         <div>
-            <h3>Toimitustavat ja -kulut:</h3>
+            <h3>Toimitustavat ja -kulut</h3>
 
-            <div>
+            <div class="ml-3">
                 <h4>Nouto varastolta</h4>
                 <p>Voit halutessasi noutaa tilauksesi varastoltamme tai sovitusta noutopaikasta. Saat ilmoituksen kun
                     voit noutaa tilauksesi. Nouto on maksuton. Henkilöllisyys pitää todistaa noudon yhteydessä.
-            </div>
-            <div>
-                <h4>Nouto postista</h4>
-                Toimitus lähimpään postin toimituspaikkaan 5-12€/lähetys. Toimituksen hinta riippuu paketin koosta
-                ja painosta. Toimitetaan lähimpään postiin, josta voit paketin noutaa saatuasi saapumisilmoituksen.
-                Saat ohjeet noutamiseen tekstiviestillä paketin saapuessa. Henkilöllisyys pitää todistaa noudon
-                yhteydessä.</p>
+
+
+                    <h4>Nouto postista</h4>
+                    Toimitus lähimpään postin toimituspaikkaan 5-12€/lähetys. Toimituksen hinta riippuu paketin
+                    koosta
+                    ja painosta. Toimitetaan lähimpään postiin, josta voit paketin noutaa saatuasi
+                    saapumisilmoituksen.
+                    Saat ohjeet noutamiseen tekstiviestillä paketin saapuessa. Henkilöllisyys pitää todistaa noudon
+                    yhteydessä.</p>
+
             </div>
         </div>
         <div>
             <h3>Toimitusaika</h3>
-            <p>Tilauksesi lähetetään 1-4 arkipäivän kuluessa. Mikäli tuote on loppunut varastostamme, otamme sinuun
-                yhteyttä mahdollisimman nopeasti. Emme ole vastuussa Postin kuljetusviiveistä tai mahdollisista
-                muutoksista ja niiden seurauksista.</p>
+            <div class="ml-3">
+                <p>Tilauksesi lähetetään 1-4 arkipäivän kuluessa. Mikäli tuote on loppunut varastostamme, otamme sinuun
+                    yhteyttä mahdollisimman nopeasti. Emme ole vastuussa Postin kuljetusviiveistä tai mahdollisista
+                    muutoksista ja niiden seurauksista.</p>
+            </div>
         </div>
+
 
         <div>
             <h3>Ostoskori</h3>
         </div>
-        <div>
+        <div class="ml-3">
             <p>Verkkokauppamme toimii ostoskori-periaatteella. Ostoskorin löydät selaimen oikeasta ylänurkasta tai
                 mobiililaitteella vasemmalta avautuvan navigointivalikon pohjalta.
                 Ostoskorissa voit:</p>
@@ -51,44 +60,68 @@
             </ul>
             <p>Klikkaamalla ostoskorissa olevaa tuotetta pääset selaamaan tuotteen tietoja.</p>
         </div>
+
         <div>
             <h3>Vaihto-, palautus- ja peruutusoikeus</h3>
+        </div>
+        <div class="ml-3">
             <p>Tästä verkkokaupasta ostamasi tuotteet ovat elintarviketuotteita, jolloin tuotteilla ei ole
                 palautusoikeutta. Jos tuotetta ei ole noudettu, tilaus on silti voimassa. Veloitamme
-                uudelleenlähetyksestä 20€. Tietyissä tilanteissa kun tuote tai tilaus on viallinen, niin palautus tai
-                vaihtaminen on maksutonta. Mikäli paketti tai sen sisältö on kuljetuksessa vioittunut, tee ilmoitus
+                uudelleenlähetyksestä 20€. Tietyissä tilanteissa kun tuote tai tilaus on viallinen, niin
+                palautus
+                tai
+                vaihtaminen on maksutonta. Mikäli paketti tai sen sisältö on kuljetuksessa vioittunut, tee
+                ilmoitus
                 postille vastaanottaessasi pakettia, minkä jälkeen ota yhteys asiakaspalveluumme!</p>
         </div>
         <div>
-            <h3>Tilaaminen</h3>
-            <p>Verkkokauppaamme voit halutessasi rekisteröityä, mutta tilaaminen onnistuu myös ilman kirjautumista.Kun
-                olet lisännyt ostoskoriin kaiken haluamasi paina ostoskori painiketta selaimessa oikealla ylhäällä.
-                Varmista, että haluamasi tuotteet ovat ostoskorissa. Paina sitten tilaa-painiketta ja täytä
-                tilauslomakkeeseen tarvittavat tiedot (toimitustapa, maksutapa, omat tiedot). Tämän jälkeen voit maksaa
-                ostoksesi.
-            </p>
-        </div>
-        <div>
-            <h3>Maksutavat</h3>
-
-            <p>Voit valita seuraavista maksutavoista:</p>
-            <ul>
-                <li>Klarna</li>
-                <li>Verkkopankki</li>
-                <li>Luottokortti</li>
-                <li>Mobilepay</li>
-            </ul>
-            <p>Kaikki maksutavat ovat yhtä turvallisia.</p>
-        </div>
-
-        <div>
-            <h3>Asiakastietojen säilyttäminen ja luovutus</h3>
-            <p>Käsittelemme kaikki asiakastiedot täysin luottamuksellisesti ja sitoudumme olemaan luovuttamatta
-                asiakastietoja kolmannelle osapuolelle.</p>
-        </div>
-        <div>
-            <h3>Kysyttävää?</h3>
             <div>
+                <h3>Tilaaminen</h3>
+            </div>
+            <div class="ml-3">
+                <p>Verkkokauppaamme voit halutessasi rekisteröityä, mutta tilaaminen onnistuu myös ilman
+                    kirjautumista.Kun
+                    olet lisännyt ostoskoriin kaiken haluamasi paina ostoskori painiketta selaimessa oikealla
+                    ylhäällä.
+                    Varmista, että haluamasi tuotteet ovat ostoskorissa. Paina sitten tilaa-painiketta ja täytä
+                    tilauslomakkeeseen tarvittavat tiedot (toimitustapa, maksutapa, omat tiedot). Tämän jälkeen
+                    voit
+                    maksaa
+                    ostoksesi.
+                </p>
+            </div>
+        </div>
+        <div>
+            <div>
+                <h3>Maksutavat</h3>
+            </div>
+            <div class="ml-3">
+                <p>Voit valita seuraavista maksutavoista:</p>
+                <ul>
+                    <li>Klarna</li>
+                    <li>Verkkopankki</li>
+                    <li>Luottokortti</li>
+                    <li>Mobilepay</li>
+                </ul>
+                <p>Kaikki maksutavat ovat yhtä turvallisia.</p>
+            </div>
+        </div>
+
+        <div>
+            <div>
+                <h3>Asiakastietojen säilyttäminen ja luovutus</h3>
+            </div>
+            <div class="ml-3">
+                <p>Käsittelemme kaikki asiakastiedot täysin luottamuksellisesti ja sitoudumme olemaan
+                    luovuttamatta
+                    asiakastietoja kolmannelle osapuolelle.</p>
+            </div>
+        </div>
+        <div>
+            <div>
+                <h3>Kysyttävää?</h3>
+            </div>
+            <div class="ml-3">
                 <h4>Ota yhteyttä:</h4>
                 <p>Herkkuhetki OY<br>
                     Joutsenkatu 12<br>
