f1e7cd654ecd4cfa3a7ff8690c8acdcab7b5acd7
Jonne Tuohimaa <n9tujo00@students.oamk.fi>
2020-04-16 13:34:23 +0300

    admin näkymässä voi rajata tilauksia niiden tilan perusteella.

--- app/Views/admin/orders_view.php
+++ app/Views/admin/orders_view.php
@@ -1,8 +1,20 @@
 <div class="row">
     <div class="col">
         <h1>Tehdyt tilaukset:</h1>
+        <form method="post" action="<?= site_url('admin/sortbystatus/')?>">
+        <div class="row">
+            <div class="form-group col-3 mt-2" style="margin-bottom: 0rem;">
+                <p>Rajaa tilan mukaan:
+                <select class="form-control" id="limitbystatus" name="status"></p>
+                    <option value="ordered">Tilattu</option>
+                    <option value="paid">Maksettu</option>
+                    <option value="shipped">Toimitettu</option>
+                </select>
+            </div>
+        </div>
+        <button type="submit" class="btn btn-danger mb-2">Rajaa</button>
+        </form>
         <table class="table table-striped table-sm">
-
         <tr>
             <th>Etunimi</th>
             <th>Sukunimi</th>
@@ -32,7 +44,7 @@
                     <td class="m-3"><?= anchor('admin/showOrder/' . $order['id'], ' <button>Näytä tilaus</button>')?></td>
                     <td class="m-3"><?= anchor('admin/updateStatus/' . $order['id'], ' <button>Muokkaa tilaa</button>')?></td>
                 </tr>            
-                <?php endforeach ?>     
+                <?php endforeach; ?>     
         </table>
     </div>
 </div>
